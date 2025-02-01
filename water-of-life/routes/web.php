10317789090754960e49b17e2819f2dd2ec8d40e
Briap Sovath <mrbriap@gmail.com>
2025-02-01 15:30:45 +0700

    life

---
+++ water-of-life/routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/index', [IndexController::class, 'index'])->name('index.index');
---
+++ water-of-life/tailwind.config.js
@@ -0,0 +1,18 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import tailwindcss from '@tailwindcss/vite'
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
+};
